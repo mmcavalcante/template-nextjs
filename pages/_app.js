ab201a2bd4c27c5e9073bc2728bcec155794346b
mmcavalcante <marciomendescavalcante@gmail.com>
2022-02-24 14:07:14 -0300

    1 file modified

--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,7 @@ function CsDev({ Component, pageProps }) {
         <title>csolution.dev</title>
       </Head>
 
-      <img src="./public/logo.png"></img>
+      <img src="./public/icon.png"></img>
 
       <header>CABECALHO</header>
       <Link href="/">
